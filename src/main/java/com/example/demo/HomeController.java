f809c22b4833c1973cce359a6d2c21f5b9dfd3bf
Pedro <alumno.programadorduoc@gmail.com>
2024-07-26 03:23:14 -0400

    se añade la api de personas

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
 
 
-    @GetMapping("/hola")
+    @GetMapping("/personas")
     public String index(){
         return "hola a todos";
     }
