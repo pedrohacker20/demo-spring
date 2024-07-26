92c0745b0195d728d0f58fd4f0a09243918517bf
Pedro <alumno.programadorduoc@gmail.com>
2024-07-26 03:12:42 -0400

    se añade requestmapping

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -2,9 +2,11 @@ package com.example.demo;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class HomeController {
 
 
